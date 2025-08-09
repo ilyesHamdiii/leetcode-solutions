188a646538d52fb380fb65ead78d606fa148dad2
Ilyes HAMDI <ilyesilyeshamdi10@gmail.com>
2025-08-09 18:45:27 +0100

    leetcode

---
+++ 08-09-2025-Minimum Number of Arrows to Burst Balloons.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/?envType=study-plan-v2&envId=leetcode-75
+
+# Difficulty:Medium
+
+# Time: O(n log n)
+
+# Space: O(1)
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+        res=len(points)
+
+        prev=points[0]
+        for i in range(1,len(points)):
+            curr=points[i]
+            if prev[1]>=curr[0]:
+                res-=1
+                prev=[curr[0],min(curr[1],prev[1])]
+
+            else:
+                prev=curr
+        return res
+
---
+++ 08-09-2025-Non-overlapping Intervals.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/non-overlapping-intervals/description/
+# Difficulty:Medium
+
+# Time: O(n log n)
+
+# Space: O(1)
+from ast import List
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        res=0
+        prevEnd=intervals[0][1]
+        for start,end in intervals[1:]:
+            if start>=prevEnd:
+                prevEnd=end
+            else:
+                res+=1
+                prevEnd=min(prevEnd,end)
+        return res
+        
+
