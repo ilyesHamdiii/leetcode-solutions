565481d00ffead9f1b66fd74305799e946872e33
Ilyes HAMDI <ilyesilyeshamdi10@gmail.com>
2025-08-20 18:56:11 +0100

    leetcode

---
+++ 08-20-2025-Count Square Submatrices with All Ones.py
@@ -0,0 +1,26 @@
+#https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+# Difficulty:Medium
+# Time:o(n)
+# 
+# # Space:O(n)
+# Python
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        ROWS,COLS=len(matrix),len(matrix[0])
+        cache={}
+        def dfs(r,c):
+            if r==ROWS or c==COLS or not matrix[r][c]:
+                return 0
+            if (r,c) in cache:
+                return cache[(r,c)]
+            cache[(r,c)]=1+min(dfs(r+1,c),
+            dfs(r,c+1),
+            dfs(r+1,c+1),
+            
+            )
+            return cache[(r,c)]
+        res=0
+        for r in range(ROWS):
+            for c in range(COLS):
+                res+=dfs(r,c)
+        return res
