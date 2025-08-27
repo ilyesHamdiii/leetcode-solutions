81bd82243e7675af8208ceadae81a679194e8031
Ilyes HAMDI <ilyesilyeshamdi10@gmail.com>
2025-08-27 18:27:56 +0100

    leetcode

---
+++ 08-27-2025-Length of Longest V-Shaped Diagonal Segment.py
@@ -0,0 +1,28 @@
+#https://leetcode.com/problems/length-of-longest-v-shaped-diagonal-segment/description/?envType=daily-question&envId=2025-08-27
+# Difficulty:Hard
+# Time:o(n*n)
+# 
+# # Space:O(N)
+# Python
+class Solution:
+    def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
+        m,n = len(grid),len(grid[0])
+        directions = [(-1,-1),(-1,1),(1,1),(1,-1)]
+
+        @lru_cache(None)
+        def dfs(i,j,target,dir,turn):
+            if i<0 or i == m  or j<0 or j == n or grid[i][j] != target: return 0
+            target = 0 if target == 2 else 2
+            res = 1 + dfs(i+directions[dir][0],j+directions[dir][1],target,dir,turn)
+            if not turn:
+                newdir = (1 + dir)%4
+                res = max(res, 1 + dfs(i+directions[newdir][0],j+directions[newdir][1],target,newdir,not turn))
+            return res
+        
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    for d in range(4):
+                        res = max(res,1 + dfs(i+directions[d][0],j+directions[d][1],2,d,False))
+        return res
